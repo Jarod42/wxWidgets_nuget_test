ce9aa8449ef8818b340ef8128faa15caf2bab761
Jarod42 <Jarod42@users.noreply.github.com>
2026-01-31 11:31:56 +0100

    [CI] Allow to create (local) nuget package

--- premake5.lua
+++ premake5.lua
@@ -4,6 +4,12 @@ newoption {
 	description = 'Version of wxWidgets',
 }
 
+newoption {
+	trigger = 'nugetsource',
+	value = '',
+	description = 'url of nuget package source',
+}
+
 if _ACTION == nil then
 	return
 end
@@ -18,20 +24,24 @@ local LocationDir = 'solution/%{_ACTION}'
 workspace 'Project'
 	location(LocationDir)
 	configurations { 'Debug', 'Release' }
-	platforms { 'x32', 'x64' }
+	platforms { 'Win32', 'x64' }
 
 	objdir(path.join(LocationDir, 'obj')) -- premake adds $(configName)/$(AppName)
-	targetdir(path.join(LocationDir, 'bin/%{cfg.buildcfg}'))
+	targetdir(path.join(LocationDir, 'bin/%{cfg.platform}/%{cfg.buildcfg}'))
 	startproject 'app'
 
-	filter 'platforms:x32'
-		architecture 'x32'
+	filter 'platforms:Win32'
+		architecture 'x86'
+		
 	filter 'platforms:x64'
 		architecture 'x64'
+		
 	filter { 'action:vs*' }
+if _OPTIONS['nugetsource'] ~= nil then
+		nugetsource(_OPTIONS['nugetsource'])
+end
 		nuget {
-			'wxWidgets:' .. _OPTIONS['wxWidgetsVersion'],
-			--'native:0.0'
+			'wxWidgets:' .. _OPTIONS['wxWidgetsVersion']
 		}
 	filter {}
 
